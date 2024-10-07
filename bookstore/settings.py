7c44b52209aa69ca9a9209f4701ccd128a365be9
MansiS117 <msongirkar@bestpeers.com>
2024-10-07 17:08:03 +0530

    created signals for sending email

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +134,15 @@ STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     'store/static',
 ]
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+print(EMAIL_HOST_USER)  # Debugging line
+print(EMAIL_HOST_PASSWORD)  # Debugging line
