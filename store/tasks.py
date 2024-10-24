479d195a241d5825571ed680ab613dfe3a3c6545
MansiS117 <msongirkar@bestpeers.com>
2024-10-24 19:06:51 +0530

    updated the task

--- store/tasks.py
+++ store/tasks.py
@@ -1,20 +1,25 @@
 from celery import shared_task
 from django.utils import timezone
 from django.db.models import Sum
-from .models import Order, DailySalesReport, User
+from .models import OrderItem, DailySalesReport, User
+
 
 @shared_task
 def calculate_daily_sales():
     today = timezone.now().date()
-    sellers = User.objects.filter(user_type='seller')  # Assuming you have a User model with user_type field
+    sellers = User.objects.filter(user_type="seller")  # Get all sellers
 
     for seller in sellers:
-        # Calculate total sales for today for each seller
-        total_sales = Order.objects.filter(ordered_at__date=today, seller=seller).aggregate(Sum('total_price'))['total_price__sum'] or 0.00
-        
+        # Calculate total sales for today for each seller using related_name 'order_items'
+        total_sales = (
+            OrderItem.objects.filter(
+                order__ordered_at__date=today,  # Filter by the order date
+                seller=seller,  # Filter by the seller in the OrderItem
+            ).aggregate(total_sales=Sum("unit_price"))["total_sales"]
+            or 0.00
+        )
+
         # Update or create the daily sales report for the seller
         DailySalesReport.objects.update_or_create(
-            date=today,
-            seller=seller,  # Pass the seller instance directly
-            defaults={'total_sales': total_sales}
+            date=today, seller=seller, defaults={"total_sales": total_sales}
         )
